9be39a96957f6d04c20bd01eb6f67256e2c4c66b
Vipin Gautam <130040245+vipingautam01@users.noreply.github.com>
2023-05-20 23:20:36 +0530

    #

---
+++ style.css
@@ -0,0 +1,202 @@
+body {
+    background-color: black;
+    color: white;
+  }
+  
+  h1 {
+    text-align: center;
+    margin-top: 50px;
+  }
+  
+ .form-container{
+    margin-left: 20px;
+ }
+  
+  
+  
+  .form-group input,
+  .form-group select,
+  .form-group button {
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    font-size: 16px;
+  }
+  
+  .form-group button {
+    background-color: WHITE;
+    color: black;
+    border: none;
+  }
+  
+  .form-group button:hover {
+    background-color: white;
+
+  }
+  
+  #user-list {
+    margin-top: 1rem;
+    color: white;
+    background-color: black;
+    padding: 15px;
+  }
+  
+  #user-list > h2 {
+    margin-bottom: 1rem;
+  }
+  
+  label {
+    margin-bottom:1px;
+  }
+  
+  input[type="text"],
+  input[type="number"] {
+    padding: 5px;
+    border-radius: 5px;
+    border: none;
+    margin-bottom: 5px;
+  }
+  
+  select {
+    padding: 5px;
+    border-radius: 5px;
+    border: none;
+    margin-bottom: 5px;
+  }
+  
+  button {
+    padding: 5px 10px;
+    border-radius: 5px;
+    border: none;
+    background-color: white;
+    color: black;
+    cursor: pointer;
+    margin-top: 10px;
+  }
+  
+  button:hover {
+    background-color: #ccc;
+  }
+  
+  #user-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  #user-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 15px;
+  }
+  
+  .user-card {
+    border: 1px solid #ccc;
+    padding: 0px 20px;
+    margin-bottom: 10px;
+    width: 400px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .user-card .user-info {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  
+  .user-card .user-info h2 {
+    margin: 0;
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+  
+  .user-card .user-info p {
+    margin: 0;
+    font-size: 16px;
+  }
+  
+  .user-card .user-info span {
+    font-weight: bold;
+  }
+  #filter-btn {
+    margin-left: 20px;
+    margin-top: 20px;;
+    display: block;
+    padding: 10px 30px;
+    font-size: 20px;
+  }
+  select{
+    background-color: black;
+    color: white;
+  }
+  .form-group1{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    margin-left: 10px;
+    margin-right: 10px;
+    align-items: center;
+    align-content: center;
+    
+
+  }
+
+  .form-group input{
+    background-color: black;
+    color: white;
+    border: 1px solid white;
+    width: 300px;
+    height:20px;
+
+  }
+#name{
+    margin-right: 35px;
+    background-color: black;
+    color: white;
+    border: 1px solid white;
+}
+#profession-add{
+    margin-right:35px;
+    margin-left: 35px;
+    background-color: black;
+    color: white;
+    border: 1px solid white;
+}
+#age{
+    margin-left: 35px;
+    margin-right: 35px;
+    background-color: black;
+    color: white;
+    border: 1px solid white;
+}
+#lab1{
+    justify-self: left;
+}
+#lab2{
+    justify-self: left;
+    margin-left: 35px;
+}
+
+#lab3{
+    justify-self: left;
+    margin-left: 35px;
+}
+
+.class{
+    font-weight: 300px;
+}
+#add-btn{
+    margin:30px 0;
+    width: 160px;
+    height: 40px;
+}
+#head{
+    margin:15px;
+
+}
+.form-group{
+    display: block;
+}
